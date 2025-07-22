65ef175b42a447072cbb72a6f622db4cfcb69156
JyothsnaBhardwaj <jyothsnang79@gmail.com>
2025-07-23 00:39:19 +0530

    Diamond pattern complete

--- Pattern_Printing/diamond.cpp
+++ Pattern_Printing/diamond.cpp
@@ -5,9 +5,25 @@ int main(){
     cout<< "Enter no.:"<< endl;
     cin>> n;
     int k;
-    for(int i=1; i<n; i++, k=0){
-        for(int j=1; j<=i; j++){
+    for(int i=1; i<=n; i++, k=0){
+        for(int j=1; j<=n-i; j++){
             cout<<" ";
         }
+        while(k!=2*i-1){
+            cout<<"*";
+            k++;
+        }
+        cout<<endl;
     }
+    for(int i=n-1; i>=1; i--, k=0){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        while(k!=2*i-1){
+            cout<<"*";
+            k++;
+        }
+        cout<<endl;
+    }
+    return 0;
 }
